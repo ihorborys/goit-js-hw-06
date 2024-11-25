60cc7c4716f414d4c80ab30b1f96345dc740af37
ihorborys <ihorborys@gmail.com>
2024-11-25 21:44:23 +0200

    Edit: task-2, task-3.

--- js/task-2.js
+++ js/task-2.js
@@ -1,19 +1,21 @@
 class Storage {
+    #items;
+
     constructor(items) {
-        this.items = items;
+        this.#items = items;
     }
 
     getItems() {
-        return this.items;
+        return this.#items;
     }
 
     addItem(newItem) {
-        this.items.push(newItem);
+        this.#items.push(newItem);
     }
 
     removeItem(itemToRemove) {
-        if (this.items.includes(itemToRemove)) {
-            this.items.splice(this.items.indexOf(itemToRemove), 1);
+        if (this.#items.includes(itemToRemove)) {
+            this.#items.splice(this.#items.indexOf(itemToRemove), 1);
         }
     }
 }
